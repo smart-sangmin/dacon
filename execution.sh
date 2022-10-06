2091a96e6a8f59aef65397e0cd0089af9a061c3f
smart-sangmin <70051888+smart-sangmin@users.noreply.github.com>
2022-10-06 13:16:39 +0900

    Update execution.sh

--- execution.sh
+++ execution.sh
@@ -19,7 +19,7 @@
 # # # python create_lmdb_dataset.py --inputPath ./data/not_lmdb --gtFile data/test_vertical.csv --outputPath data/test_vertical_rotate_lmdb_dataset
 
 # train.py 실행
-# python train.py --imgH 160 --imgW 500 --train_data /home/lab/sangminnim/train_horizontal_lmdb_dataset --valid_data /home/lab/sangminnim/val_horizontal_lmdb_dataset --manualSeed 1 --saved_model /home/lab/sangminnim/pretrained/horizontal/best_accuracy.pth --FT --batch_size 16 --num_iter 500000 --valInterval 1000 --select_data basic --batch_ratio 1.0 --batch_max_length 30 --data_filtering_off --Transformation TPS --FeatureExtraction VGG --SequenceModeling BiLSTM --Prediction Attn;
+# python train.py --imgH 160 --imgW 500 --train_data /home/lab/sangminnim/train_horizontal_lmdb_dataset --valid_data /home/lab/sangminnim/val_horizontal_lmdb_dataset --manualSeed 1 --saved_model /home/lab/sangminnim/pretrained/horizontal/best_accuracy.pth --FT --batch_size 64 --num_iter 500000 --valInterval 1000 --select_data basic --batch_ratio 1.0 --batch_max_length 30 --data_filtering_off --Transformation TPS --FeatureExtraction VGG --SequenceModeling BiLSTM --Prediction Attn;
 python train.py --imgH 160 --imgW 500 --train_data /home/lab/sangminnim/train_vertical_rotate_lmdb_dataset --valid_data /home/lab/sangminnim/val_vertical_rotate_lmdb_dataset --manualSeed 3 --saved_model /home/lab/sangminnim/pretrained/vertical/best_accuracy.pth --FT --batch_size 64 --num_iter 500000 --valInterval 1000 --select_data basic --batch_ratio 1.0 --batch_max_length 30 --data_filtering_off --Transformation TPS --FeatureExtraction VGG --SequenceModeling BiLSTM --Prediction Attn;
 
 # test.py 실행 (test.py에 validation func)
